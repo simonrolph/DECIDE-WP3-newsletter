71d8d0fefdcd4ef2ade269016381e5cab3390f2d
Simon Rolph <simon.p.rolph@gmail.com>
2022-08-03 09:48:27 +0100

    Add folder creation to slurm script

--- 2_render_newsletter_slurm.R
+++ 2_render_newsletter_slurm.R
@@ -3,6 +3,11 @@ args = commandArgs(trailingOnly=TRUE); i <- args[1]
 library(rmarkdown)
 library(blastula)
 
+#create a folder for the day's newsletters
+if(!dir.exists(paste0("newsletters/",Sys.Date()))){
+  dir.create(paste0("newsletters/",Sys.Date()))
+}
+
 markdown_params_list <- readRDS("data_personal/markdown_params_list.rds")
 
 print(markdown_params_list[[i]]$out)
