c92f3d363d1d8e51ba1b2112c8694e4e2d88d0f3
Simon Rolph <simon.p.rolph@gmail.com>
2022-08-03 09:58:01 +0100

    Add slurm err and out folders to gitignore and add some print statements to slurm script

--- 2_render_newsletter_slurm.R
+++ 2_render_newsletter_slurm.R
@@ -10,8 +10,10 @@ if(!dir.exists(paste0("newsletters/",Sys.Date()))){
 
 markdown_params_list <- readRDS("data_personal/markdown_params_list.rds")
 
+print("FILE SAVING TO:")
 print(markdown_params_list[[i]]$out)
   
+print("RENDERING FILE:")
 render(
   "newsletter_templates/v0_0_7.Rmd",
   output_file = markdown_params_list[[i]]$out,
@@ -20,3 +22,5 @@ render(
   envir = new.env(),
   quiet=F
 )
+
+print("END OF SCRIPT")
